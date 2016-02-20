29efd2891f9e108a75cb4b8b902ef623b6cae354
sqrtofsaturn <sqrt@octoblu.com>
2016-02-19 17:31:14 -0700

    cleanup mongo setup

--- setup/setup-mongo.js
+++ setup/setup-mongo.js
@@ -1,6 +1,7 @@
 print('inserting database values')
 var data = JSON.parse(cat('/js/setup-octoblu.json'))
 for (var i in data) {
+  data[i]._id = data[i].uuid
   db.devices.insert(data[i])
 }
 print('done')
