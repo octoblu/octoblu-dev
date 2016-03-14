60c2eca69c49df50d77b8dc53678280bb5a2dc74
sqrtofsaturn <sqrt@octoblu.com>
2016-03-14 12:09:27 -0700

    fix meshblu public key

--- tools/disable-flow.sh
+++ tools/disable-flow.sh
@@ -1,12 +1,21 @@
 #https://gist.github.com/sqrtofsaturn/3c374ee9a49c05048112
 #https://app.compose.io/octoblu/deployments/octoblu-us-west-2/mongodb/databases/meshblu/collections/devices/documents
 
-
+FLOW_ID=$1
+INSTANCE_ID=$1
 docker rm -f aaron-redis
 
 REDIS_CMD="docker run --name aaron-redis --rm -it redis redis-cli -h engine-simple.csy8op.0001.usw2.cache.amazonaws.com"
 
 ENGINE_INPUT_DATA= $REDIS_CMD hget $FLOW_ID $INSTANCE_ID/engine-input/config
 echo $ENGINE_INPUT_DATA
-$REDIS_CMD hset $FLOW_ID $INSTANCE_ID/engine-input/config-bak $ENGINE_INPUT_DATA
-$REDIS_CMD hset $FLOW_ID $INSTANCE_ID/engine-input/config "{}"
+# $REDIS_CMD hset $FLOW_ID $INSTANCE_ID/engine-input/config-bak $ENGINE_INPUT_DATA
+# $REDIS_CMD hset $FLOW_ID $INSTANCE_ID/engine-input/config "{}"
+
+
+
+REDIS_CMD="docker run --name aaron-redis --rm -it redis redis-cli -h engine-simple.csy8op.0001.usw2.cache.amazonaws.com"
+
+$REDIS_CMD hget $FLOW_ID $INSTANCE_ID/router/config
+$REDIS_CMD hset $FLOW_ID $INSTANCE_ID/router/config-bak $ENGINE_INPUT_DATA
+$REDIS_CMD hset $FLOW_ID $INSTANCE_ID/router/config "{}"
