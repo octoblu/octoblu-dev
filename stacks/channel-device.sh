eba20bca257d84619dde63bdbc7c114d7ca1c602
Erik Wilson <Erik.E.Wilson@gmail.com>
2016-04-07 14:58:38 -0700

    rename variables

--- stacks/channel-device.sh
+++ stacks/channel-device.sh
@@ -3,12 +3,12 @@
 eval $(docker-machine env --shell=bash octoblu-dev)
 SERVICES="$HOME/Projects/Octoblu/octoblu-dev/services"
 SESSION='channel-device'
-DOCKER_OR_LOCAL="run-service-docker.sh"
+RUN_SERVICE="run-service-docker.sh"
 
 while getopts ":l" opt; do
   case $opt in
     l)
-      DOCKER_OR_LOCAL="run-service-local.sh"
+      RUN_SERVICE="run-service-local.sh"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -24,7 +24,7 @@ tmux set-environment -t $SESSION DOCKER_CERT_PATH $DOCKER_CERT_PATH
 tmux set-environment -t $SESSION DOCKER_TLS_VERIFY $DOCKER_TLS_VERIFY
 tmux set-environment -t $SESSION DOCKER_HOST $DOCKER_HOST
 tmux set-environment -t $SESSION DOCKER_MACHINE_NAME $DOCKER_MACHINE_NAME
-tmux set-environment -t $SESSION DOCKER_OR_LOCAL $DOCKER_OR_LOCAL
+tmux set-environment -t $SESSION RUN_SERVICE $RUN_SERVICE
 
 tmux new-window -t $SESSION:1 -n oauth
 tmux new-window -t $SESSION:2 -n mailer
@@ -34,13 +34,13 @@ tmux send-keys -t $SESSION:0.0 C-m
 tmux send-keys -t $SESSION:0.0 "tmux kill-session -t $SESSION"
 
 tmux send-keys -t $SESSION:1.0 'cd ~/Projects/Octoblu/oauth-provider' C-m
-tmux send-keys -t $SESSION:1.0 'eval $SERVICES/$DOCKER_OR_LOCAL oauth-provider' C-m
+tmux send-keys -t $SESSION:1.0 'eval $SERVICES/$RUN_SERVICE oauth-provider' C-m
 
 tmux send-keys -t $SESSION:2.0 'cd ~/Projects/Octoblu/device-mailer' C-m
-tmux send-keys -t $SESSION:2.0 'eval $SERVICES/$DOCKER_OR_LOCAL device-mailer' C-m
+tmux send-keys -t $SESSION:2.0 'eval $SERVICES/$RUN_SERVICE device-mailer' C-m
 
 tmux send-keys -t $SESSION:3.0 'cd ~/Projects/Octoblu/device-editor-octoblu' C-m
-tmux send-keys -t $SESSION:3.0 'eval $SERVICES/$DOCKER_OR_LOCAL device-editor-octoblu' C-m
+tmux send-keys -t $SESSION:3.0 'eval $SERVICES/$RUN_SERVICE device-editor-octoblu' C-m
 
 tmux select-window -t $SESSION:1
 tmux attach-session -t $SESSION
--- stacks/octoblu.sh
+++ stacks/octoblu.sh
@@ -3,12 +3,12 @@
 eval $(docker-machine env --shell=bash octoblu-dev)
 SERVICES="$HOME/Projects/Octoblu/octoblu-dev/services"
 SESSION='octoblu'
-DOCKER_OR_LOCAL="run-service-docker.sh"
+RUN_SERVICE="run-service-docker.sh"
 
 while getopts ":l" opt; do
   case $opt in
     l)
-      DOCKER_OR_LOCAL="run-service-local.sh"
+      RUN_SERVICE="run-service-local.sh"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -24,7 +24,7 @@ tmux set-environment -t $SESSION DOCKER_CERT_PATH $DOCKER_CERT_PATH
 tmux set-environment -t $SESSION DOCKER_TLS_VERIFY $DOCKER_TLS_VERIFY
 tmux set-environment -t $SESSION DOCKER_HOST $DOCKER_HOST
 tmux set-environment -t $SESSION DOCKER_MACHINE_NAME $DOCKER_MACHINE_NAME
-tmux set-environment -t $SESSION DOCKER_OR_LOCAL $DOCKER_OR_LOCAL
+tmux set-environment -t $SESSION RUN_SERVICE $RUN_SERVICE
 
 tmux new-window -t $SESSION:1 -n app
 tmux new-window -t $SESSION:2 -n api
@@ -35,16 +35,16 @@ tmux send-keys -t $SESSION:0.0 C-m
 tmux send-keys -t $SESSION:0.0 "tmux kill-session -t $SESSION"
 
 tmux send-keys -t $SESSION:1.0 'cd ~/Projects/Octoblu/app-octoblu' C-m
-tmux send-keys -t $SESSION:1.0 'eval $SERVICES/$DOCKER_OR_LOCAL app-octoblu' C-m
+tmux send-keys -t $SESSION:1.0 'eval $SERVICES/$RUN_SERVICE app-octoblu' C-m
 
 tmux send-keys -t $SESSION:2.0 'cd ~/Projects/Octoblu/api-octoblu' C-m
-tmux send-keys -t $SESSION:2.0 'eval $SERVICES/$DOCKER_OR_LOCAL api-octoblu' C-m
+tmux send-keys -t $SESSION:2.0 'eval $SERVICES/$RUN_SERVICE api-octoblu' C-m
 
 tmux send-keys -t $SESSION:3.0 'cd ~/Projects/Octoblu/meshblu-authenticator-email-password' C-m
-tmux send-keys -t $SESSION:3.0 'eval $SERVICES/$DOCKER_OR_LOCAL meshblu-authenticator-email-password' C-m
+tmux send-keys -t $SESSION:3.0 'eval $SERVICES/$RUN_SERVICE meshblu-authenticator-email-password' C-m
 
 tmux send-keys -t $SESSION:4.0 'cd ~/Projects/Octoblu/email-password-site' C-m
-tmux send-keys -t $SESSION:4.0 'eval $SERVICES/$DOCKER_OR_LOCAL email-password-site' C-m
+tmux send-keys -t $SESSION:4.0 'eval $SERVICES/$RUN_SERVICE email-password-site' C-m
 
 tmux select-window -t $SESSION:1
 tmux attach-session -t $SESSION
