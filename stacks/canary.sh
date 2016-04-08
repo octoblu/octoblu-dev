38b530908ef451fe2f7e8ec448d026bb3f33c629
Erik Wilson <Erik.E.Wilson@gmail.com>
2016-04-07 22:43:52 -0700

    determine machine capabilities and prefer local dev for smaller environments

--- stacks/canary.sh
+++ stacks/canary.sh
@@ -1,36 +1,11 @@
 #!/usr/bin/env bash
 
-eval $(docker-machine env --shell=bash octoblu-dev)
-SERVICES="$HOME/Projects/Octoblu/octoblu-dev/services"
+cd $(dirname $0)
 SESSION='canary'
-RUN_SERVICE="run-service-docker.sh"
-
-while getopts ":l" opt; do
-  case $opt in
-    l)
-      RUN_SERVICE="run-service-local.sh"
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
-  esac
-done
-
-tmux start-server
-tmux new-session -d -s $SESSION -n √ø
-
-tmux set-environment -t $SESSION SERVICES $SERVICES
-tmux set-environment -t $SESSION DOCKER_CERT_PATH $DOCKER_CERT_PATH
-tmux set-environment -t $SESSION DOCKER_TLS_VERIFY $DOCKER_TLS_VERIFY
-tmux set-environment -t $SESSION DOCKER_HOST $DOCKER_HOST
-tmux set-environment -t $SESSION DOCKER_MACHINE_NAME $DOCKER_MACHINE_NAME
-tmux set-environment -t $SESSION RUN_SERVICE $RUN_SERVICE
+. ./_defaults.sh
 
 tmux new-window -t $SESSION:1 -n canary
 tmux new-window -t $SESSION:2 -n triggers
-
-tmux send-keys -t $SESSION:0.0 C-m
-tmux send-keys -t $SESSION:0.0 "tmux kill-session -t $SESSION"
 
 tmux send-keys -t $SESSION:1.0 "cd ~/Projects/Octoblu/flow-canary" C-m
 tmux send-keys -t $SESSION:1.0 "eval \$SERVICES/$RUN_SERVICE flow-canary" C-m
--- stacks/meshblu.sh
+++ stacks/meshblu.sh
@@ -1,30 +1,8 @@
 #!/usr/bin/env bash
 
-eval $(docker-machine env --shell=bash octoblu-dev)
-SERVICES="$HOME/Projects/Octoblu/octoblu-dev/services"
+cd $(dirname $0)
 SESSION='meshblu'
-RUN_SERVICE="run-service-docker.sh"
-
-while getopts ":l" opt; do
-  case $opt in
-    l)
-      RUN_SERVICE="run-service-local.sh"
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      ;;
-  esac
-done
-
-tmux start-server
-tmux new-session -d -s $SESSION -n √ø
-
-tmux set-environment -t $SESSION SERVICES $SERVICES
-tmux set-environment -t $SESSION DOCKER_CERT_PATH $DOCKER_CERT_PATH
-tmux set-environment -t $SESSION DOCKER_TLS_VERIFY $DOCKER_TLS_VERIFY
-tmux set-environment -t $SESSION DOCKER_HOST $DOCKER_HOST
-tmux set-environment -t $SESSION DOCKER_MACHINE_NAME $DOCKER_MACHINE_NAME
-tmux set-environment -t $SESSION RUN_SERVICE $RUN_SERVICE
+. ./_defaults.sh
 
 tmux new-window -t $SESSION:1 -n proxy
 tmux new-window -t $SESSION:2 -n frontends
@@ -36,9 +14,6 @@ tmux split-window -t $SESSION:2 -d -p 50
 
 tmux split-window -t $SESSION:3 -d -p 33
 tmux split-window -t $SESSION:3 -d -p 50
-
-tmux send-keys -t $SESSION:0.0 C-m
-tmux send-keys -t $SESSION:0.0 "tmux kill-session -t $SESSION"
 
 tmux send-keys -t $SESSION:1.0 "cd ~/Projects/Octoblu/meshblu-haproxy" C-m
 tmux send-keys -t $SESSION:1.0 "eval \$SERVICES/run-service-docker.sh meshblu-haproxy" C-m
