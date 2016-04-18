352e45008b285414bec60b03be6c6850bd60f383
Square Root of Saturn <sqrt@octoblu.com>
2016-04-18 14:43:51 -0700

    working amqp

--- services-aux/rabbitmq/stop.sh
+++ services-aux/rabbitmq/stop.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 eval $(docker-machine env --shell bash octoblu-dev)
+export DNS=0.0.0.0
 
 echo - rabbitmq
-docker-compose rm --all --force
+docker-compose stop
+docker-compose rm --force --all
