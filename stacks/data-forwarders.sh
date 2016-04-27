84bf14aba547350f6b42a5c871a069abead6f889
miql <mxander@gmail.com>
2016-04-27 13:13:47 -0700

    Add oauth-provider to data-forwarder stack and a forwarder oauth device

--- stacks/data-forwarders.sh
+++ stacks/data-forwarders.sh
@@ -6,12 +6,16 @@ SESSION='data-forwarders'
 
 tmux new-window -t $SESSION:1 -n data-forwarders
 tmux new-window -t $SESSION:2 -n event-collector
+tmux new-window -t $SESSION:3 -n oauth-provider
 
 tmux send-keys -t $SESSION:1.0 "cd ~/Projects/Octoblu/data-forwarders" C-m
 tmux send-keys -t $SESSION:1.0 "eval \$SERVICES/$RUN_SERVICE data-forwarders" C-m
 
 tmux send-keys -t $SESSION:2.0 "cd ~/Projects/Octoblu/meshblu-splunk-event-collector" C-m
 tmux send-keys -t $SESSION:2.0 "eval \$SERVICES/$RUN_SERVICE meshblu-splunk-event-collector" C-m
+
+tmux send-keys -t $SESSION:3.0 "cd ~/Projects/Octoblu/oauth-provider" C-m
+tmux send-keys -t $SESSION:3.0 "eval \$SERVICES/$RUN_SERVICE oauth-provider" C-m
 
 tmux select-window -t $SESSION:1
 tmux attach-session -t $SESSION
