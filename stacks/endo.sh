4e44c4880cf23c2a318afbbae81446e743fca583
sqrt <sqrt@octoblu.com>
2016-06-16 09:50:56 -0700

    Add endo-sendgrid

--- stacks/endo.sh
+++ stacks/endo.sh
@@ -1,17 +1,43 @@
 #!/usr/bin/env bash
 
-cd $(dirname $0)
 SESSION='endo'
-. ./_defaults.sh
 
-tmux new-window -t $SESSION:1 -n manager
-tmux new-window -t $SESSION:2 -n endos
+change_directory(){
+  cd $(dirname $0)
+}
 
-tmux send-keys -t $SESSION:1.0 "cd ~/Projects/Octoblu/endo-manager" C-m
-tmux send-keys -t $SESSION:1.0 "eval \$SERVICES/$RUN_SERVICE endo-manager" C-m
+join_existing_session(){
+  tmux attach-session -t "$SESSION"
+}
 
-tmux send-keys -t $SESSION:2.0 "cd ~/Projects/Octoblu/endo-github" C-m
-tmux send-keys -t $SESSION:2.0 "eval \$SERVICES/$RUN_SERVICE endo-github" C-m
+start_new(){
+  . ./_defaults.sh
 
-tmux select-window -t $SESSION:1
-tmux attach-session -t $SESSION
+  tmux new-window -t $SESSION:1 -n UI
+  tmux new-window -t $SESSION:2 -n github
+  tmux new-window -t $SESSION:3 -n sendgrid
+
+  tmux split-window -t $SESSION:1 -d -p 50
+
+  tmux send-keys -t $SESSION:1.0 "cd ~/Projects/Octoblu/endo-manager" C-m
+  tmux send-keys -t $SESSION:1.0 "eval \$SERVICES/$RUN_SERVICE endo-manager" C-m
+
+  tmux send-keys -t $SESSION:1.1 "cd ~/Projects/Octoblu/form-service" C-m
+  tmux send-keys -t $SESSION:1.1 "eval \$SERVICES/$RUN_SERVICE form-service" C-m
+
+  tmux send-keys -t $SESSION:2.0 "cd ~/Projects/Octoblu/endo-github" C-m
+  tmux send-keys -t $SESSION:2.0 "eval \$SERVICES/$RUN_SERVICE endo-github" C-m
+
+  tmux send-keys -t $SESSION:3.0 "cd ~/Projects/Octoblu/endo-sendgrid" C-m
+  tmux send-keys -t $SESSION:3.0 "eval \$SERVICES/$RUN_SERVICE endo-sendgrid" C-m
+
+  tmux select-window -t $SESSION:1
+  tmux attach-session -t $SESSION
+}
+
+
+main(){ 
+  change_directory 
+  join_existing_session || start_new
+}
+main $@
